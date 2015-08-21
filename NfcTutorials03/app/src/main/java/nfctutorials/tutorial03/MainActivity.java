38096724521344fe18ee51e76e915afd6729ae7a
Tamsyn Murphy <tamsyn.murphy@gmail.com>
2015-08-21 12:42:53 +0100

    NFC Tutorial 3 - writing a small prog that writes a string onto the NFC tag

---
+++ NfcTutorials03/app/src/main/java/nfctutorials/tutorial03/MainActivity.java
@@ -0,0 +1,175 @@
+package nfctutorials.tutorial03;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NdefFormatable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    NfcAdapter nfcAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        enableForegroundDispatchSystem();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        disableForegroundDispatchSystems();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        if(intent.hasExtra(NfcAdapter.EXTRA_TAG))
+        {
+            Toast.makeText(this, "NfcIntent!", Toast.LENGTH_SHORT).show();
+
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            NdefMessage ndefMessage = createNdefMessage("My string content!");
+
+            writeNdefMessage(tag, ndefMessage);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void enableForegroundDispatchSystem() {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        IntentFilter[] intentFilter = new IntentFilter[]{};
+
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilter, null);
+    }
+
+    private void disableForegroundDispatchSystems() {
+
+        nfcAdapter.disableForegroundDispatch(this);
+    }
+
+    private void formatTag(Tag tag, NdefMessage ndfMessage) {
+        try
+        {
+            NdefFormatable ndefFormatable = NdefFormatable.get(tag);
+
+            if(ndefFormatable == null)
+            {
+                Toast.makeText(this, "Tag is not Ndef Formatable", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            ndefFormatable.connect();
+            NdefMessage ndefMessage = null;
+            ndefFormatable.format(ndefMessage);
+            ndefFormatable.close();
+
+            Toast.makeText(this, "Tag writtene!", Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception e)
+        {
+            Log.e("formatTag", e.getMessage());
+        }
+    }
+
+    private void writeNdefMessage(Tag tag, NdefMessage ndefMessage)
+    {
+        try
+        {
+            NdefFormatable ndefFormatable = NdefFormatable.get(tag);
+
+            if(tag == null)
+            {
+                Toast.makeText(this, "Tag object cannot be null", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Ndef ndef = Ndef.get(tag);
+
+            if(ndef == null)
+            {
+                //format tag with the ndef format and writes the message
+                formatTag(tag, ndefMessage);
+            }
+
+            else
+            {
+                ndef.connect();
+
+                if(!ndef.isWritable())
+                {
+                    Toast.makeText(this, "Tag is not writable!", Toast.LENGTH_SHORT).show();
+
+                    ndef.close();
+                    return;
+                }
+
+                ndef.writeNdefMessage(ndefMessage);
+                ndef.close();
+
+                Toast.makeText(this, "Tag written!", Toast.LENGTH_SHORT).show();
+            }
+        }
+        catch (Exception e)
+        {
+            Log.e("writeNdefMessage", e.getMessage());
+        }
+    }
+
+    private NdefMessage createNdefMessage(String content)
+    {
+        NdefRecord ndefRecord = NdefRecord.createTextRecord(content, content);
+
+        NdefMessage ndefMessage = new NdefMessage(new NdefRecord[] { ndefRecord });
+
+        return ndefMessage;
+    }
+}
